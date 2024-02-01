4c84b93c2baadcb44d2d992c4934d5b8c0a24d9f
Tyler <tyler.bernero@gmail.com>
2024-02-01 16:21:31 -0500

    Removed node_modules

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let hltb = require('howlongtobeat');
-let hltbService = new hltb.HowLongToBeatService();
+// let hltb = require('howlongtobeat');
+// let hltbService = new hltb.HowLongToBeatService();
+const express= require('express');
+const app = express();
+const port = 5000;
 
 console.log("Starting App. . .");
-hltbService.search('Nioh').then(result => console.log(result));
+app.get('/', (req, res) => {        //get requests to the root ("/") will route here
+    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile
+});
+// hltbService.search('Nioh').then(result => console.log(result));
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
