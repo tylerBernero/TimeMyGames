bdd979ef9bc14d677f0c85c782d0c0216c599e04
Tyler <tyler.bernero@gmail.com>
2024-02-13 19:42:16 -0500

    Formatting

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ const prompt = require('prompt-sync')();
 const hltb = require('howlongtobeat');
 let hltbService = new hltb.HowLongToBeatService();
 
+	var promises = [];	// array of promises
+	var doAgain = true;
+	var gameName;
+	var totalTime = 0;
+
 function main() {
 	console.log("*** Welcome to Time My Games ***");
 
-	var promises = [];	// array of promises
-	var doAgain = true;
+	console.log("Please enter the name of a game: ");
+	gameName = prompt("").toLowerCase().trim();
 
 	while(doAgain) {
-		var gameName = prompt("Please enter the name of a game: ").toLowerCase().trim();
-
 		promises.push(searchGame(gameName));
 		console.log('\nAdded ' + gameName + ' to the list\n');
-		console.log('Would you like to search for another game (1) or print list (2):');
-		doAgainPrompt = prompt("").trim();
+		console.log('Enter another name of a game or press (2) to print list:');
+		gameName = prompt("").trim();
 
-		if (doAgainPrompt === '2') {
+		if (gameName === '2') {
 			doAgain = false;
 		}
 	}
@@ -26,8 +29,11 @@ function main() {
 	Promise.all(promises)
 		.then(results => {
 			for (const resp of results) {
-				console.log(resp[0].name + ' / ' + resp[0].gameplayMain + ' hours');
+				console.log(resp[0].name + ': ' + resp[0].gameplayMain + ' hours');
+				totalTime += resp[0].gameplayMain;
 			}
+
+			console.log("\nTotal Time to Complete: " + totalTime + ' hours');
 		}).catch(error => {
 			console.log("\nError loading games / times: " + e);
 		}).finally(() => {
